c236b60d03aa4ab8a91efda93b6c8d3d661fb132
韦海晖 <franciswei98@gmail.com>
2019-11-27 11:15:43 +0800

    refactor: rm old fns

--- src/commands.ts
+++ src/commands.ts
@@ -1,28 +1,10 @@
 import Telegraf, { ContextMessageUpdate } from "telegraf";
-import { getBalance, fromWeiToEther } from "./utils/eth";
-import FrankCoin from "./token/FrankCoin";
-import { messages } from "./constant";
 
 export default (bot: Telegraf<ContextMessageUpdate>) => {
-
-    bot.command('balanceOf', async ({ message, reply }) => {
-        console.info(message)
-        const parameters: string[] = message!.text!.split(" ").slice(1)
-        const [address] = parameters
-        // @todo: 对 address 用工具进行checksum
-        if (!address || address.length !== 42) reply(messages.INVALID_ADDRESS)
-        else {
-            try {
-                const result = await FrankCoin.methods.balanceOf(address).call()
-                const replyMsg = `Balance of ${address}: ${fromWeiToEther(result)}`
-                console.info(replyMsg)
-                reply(replyMsg)
-            } catch (error) {
-                reply(`error happened: ` + error.toString())
-            }
-        }
+    // 绑定用户
+    bot.command('bind', ({ message, reply }) => {
+        reply(`todo`)
     })
-
 
     bot.command('ping', ({ message, reply }) => {
         console.info(message)
