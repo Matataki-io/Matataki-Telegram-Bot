c86c8e75932a617134ac12d39ed7787e3d2a9245
神樹桜乃 <kodama@sakuno.moe>
2019-12-16 04:02:51 +0800

    Update

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -3,7 +3,4 @@ import { MessageHandler } from "../definitions";
 export interface IController { }
 
 export class BaseController<T extends { [P in keyof T]: MessageHandler }> implements IController {
-    constructor() {
-        console.log(Object.getPrototypeOf(this).constructor.name, new Date);
-    }
 }
