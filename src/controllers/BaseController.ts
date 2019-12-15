b25b98622e4962a4e6242b5e32a311b4b4ece4fd
神樹桜乃 <kodama@sakuno.moe>
2019-12-16 04:20:07 +0800

    Add type guard to Controller decorator

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -2,5 +2,5 @@ import { MessageHandler } from "../definitions";
 
 export interface IController { }
 
-export class BaseController<T extends { [P in keyof T]: MessageHandler }> implements IController {
+export abstract class BaseController<T extends { [P in keyof T]: MessageHandler }> implements IController {
 }
--- src/decorators/Controller.ts
+++ src/decorators/Controller.ts
@@ -1,9 +1,14 @@
 import { decorate, injectable } from "inversify";
 
+import { BaseController } from "../controllers";
 import { MetadataKeys } from "../constants";
 
 export function Controller(prefix?: string): ClassDecorator {
-    return (target: any) => {
+    return (target: Function) => {
+        if (!BaseController.isPrototypeOf(target)) {
+            throw new Error("Target type must be a derived class of BaseController");
+        }
+
         decorate(injectable(), target);
 
         Reflect.defineMetadata(MetadataKeys.ControllerPrefix, prefix || "/", target);
