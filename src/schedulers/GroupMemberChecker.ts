a7899621574a78f435bb9a5414cbc61ff0ad71aa
神樹桜乃 <kodama@sakuno.moe>
2019-12-20 19:38:17 +0800

    Add scheduler to check member's balance

---
+++ src/schedulers/GroupMemberChecker.ts
@@ -0,0 +1,49 @@
+import { Scheduler, InjectRepository } from "../decorators";
+import { IScheduler } from "./IScheduler";
+import { inject } from "inversify";
+import { Injections, Tokens } from "../constants";
+import { BotService, TestAccountBalanceService } from "../services";
+import { Group } from "../entities";
+import { GroupRepository } from "../repositories";
+
+@Scheduler("0 */1 * * * *")
+export class GroupMemberChecker implements IScheduler {
+    constructor(
+        @inject(Injections.BotService) private botService: BotService,
+        @inject(Injections.TestAccountBalanceService) private tbaService: TestAccountBalanceService,
+        @InjectRepository(Group) private groupRepo: GroupRepository) {
+
+    }
+
+    async onTick() {
+        if (!this.botService.isRunning) {
+            return;
+        }
+
+        const groups = await this.groupRepo.getGroups();
+        for (const group of groups) {
+            const groupId = Number(group.id);
+            const ethRequirement = group.requirements.find(requirement => requirement.token === Tokens.Eth)?.amount ?? 0;
+
+            for (const user of group.members) {
+                const userId = Number(user.id);
+
+                if (userId !== 1019938473 && userId !== 972107339) {
+                    console.log("Not implemented now");
+                    continue;
+                }
+
+                const userInfo = await this.botService.getMember(groupId, userId);
+                if (userInfo.status !== "member") {
+                    continue;
+                }
+
+                const balance = this.tbaService.getBalance(userId);
+
+                if (balance < ethRequirement) {
+                    await this.botService.kickMember(groupId, userId);
+                }
+            }
+        }
+    }
+}
--- src/schedulers/index.ts
+++ src/schedulers/index.ts
@@ -1,6 +1,12 @@
 import { IScheduler } from "./IScheduler";
 export { IScheduler };
 
+import { GroupMemberChecker } from "./GroupMemberChecker";
+
 interface SchedulerConstructor {
     new(...args: any[]): IScheduler;
 }
+
+export const schedulers: SchedulerConstructor[] = [
+    GroupMemberChecker,
+];
