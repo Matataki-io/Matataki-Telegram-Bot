140cf2f6cd8ca95008213c9a2bd4923e8ebc5d4b
神樹桜乃 <kodama@sakuno.moe>
2019-12-24 18:46:29 +0800

    Update

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    moduleFileExtensions: [
+        "js",
+        "ts"
+    ],
+    transform: {
+        "^.+\\.ts$": "ts-jest"
+    },
+    globals: {
+        "ts-jest": {
+            tsConfig: "tsconfig.json"
+        }
+    },
+    moduleNameMapper: {
+        "^#/(.+)": "<rootDir>/src/$1"
+    },
+    testMatch: [
+        "**/tests/**/*.test.ts"
+    ]
+};
