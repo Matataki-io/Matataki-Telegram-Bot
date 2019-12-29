18b0339d51803644c1bb078d51265ba58474c49c
神樹桜乃 <kodama@sakuno.moe>
2019-12-30 05:26:48 +0800

    Update unit test

--- tests/QueryController.test.ts
+++ tests/QueryController.test.ts
@@ -47,5 +47,23 @@ describe("QueryController", () => {
 你尚未加入 Fan票 群
 你尚未建立 Fan票 群`);
         });
+        it("With both Matataki account and minetoken", async () => {
+            const ctx = createMockedContext();
+            // @ts-ignore
+            ctx.message = {
+                from: {
+                    id: 1,
+                },
+            } as Message;
+
+            const controller = createController();
+            await controller.queryStat(ctx);
+
+            expect(ctx.replyWithMarkdown).toBeCalledTimes(1);
+            expect(ctx.replyWithMarkdown).toBeCalledWith(`瞬Matataki 昵称：[李田所](http://MATATAKI/user/114514)
+Fan票 名称：[INM（银票）](http://MATATAKI/token/1919)
+你尚未加入 Fan票 群
+你尚未建立 Fan票 群`);
+        });
     });
 });
