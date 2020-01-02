416dc62b45b3f7645666b2ab9b762ced1348238f
神樹桜乃 <kodama@sakuno.moe>
2020-01-02 22:09:02 +0800

    Update

--- src/services/impls/BotServiceImpl.ts
+++ src/services/impls/BotServiceImpl.ts
@@ -90,17 +90,20 @@ export class BotServiceImpl implements IBotService {
             await ctx.reply(`欢迎使用 ${Constants.BotName}。输入 /help 可获得帮助信息`);
         });
         this.bot.help(ctx => {
-            ctx.replyWithMarkdown(`👉🏻我应该如何建立Fan票群？
-❗此功能仅向已经发行过 Fan票 的用户开放，其他用户暂不支持建立 Fan票群
-❗如果希望发行 Fan票，请先填写并提交表单 https://wj.qq.com/s2/5208015/8e5d/
+            ctx.telegram.sendMessage(ctx.chat!.id, `👉**我应该如何建立 Fan票群？**
+❗此功能仅向已经发行过Fan票的用户开放，其他用户暂不支持建立 Fan票群
+❗如果希望发行 Fan票，请先填写并提交[表单](https://wj.qq.com/s2/5208015/8e5d/)
+
+操作步骤：
 1️⃣ 在 瞬Matataki 上登录后绑定 Telegram 账号
-2️⃣ 在 TG 中搜索 @${this.botInfo?.username} 并添加为好友
-3️⃣ 在 TG 中新建一个 Group，并将 @${this.botInfo?.username} 邀请入群
-4️⃣ 在群组中将 @${this.botInfo?.username} 设置为群管理员（操作此步骤之后群组将会自动升级为超级群）
-5️⃣ 与 @${this.botInfo?.username} 私聊，输入 /mygroups 查询自己创建的群组并记录下刚才群组的 ID 信息
-6️⃣ 与 @${this.botInfo?.username} 私聊，输入 /set [群组ID] [参数]即可设置群规则（参数代表至少持有您的 Fan票 数量）
+2️⃣ 在 TG 中搜索 @${this.botInfo!.username} 并添加为好友
+3️⃣ 在 TG 中新建一个 Group，并将 @${this.botInfo!.username} 邀请入群
+4️⃣ 在群组中将 @${this.botInfo!.username} 设置为群管理员（操作此步骤之后群组将会自动升级为超级群）
+5️⃣ 与 @${this.botInfo!.username} 私聊，输入 /mygroups 查询自己创建的群组并记录下刚才群组的 ID 信息
+6️⃣ 与 @${this.botInfo!.username} 私聊，输入 /set [群组ID] [参数]即可设置群规则（参数代表至少持有您的 Fan票 数量）
+
 👨‍👩‍👦‍👦完成以上 6 步操作即可完成 Fan票 群建立,已经建立过的 Fan票 群组将会显示在 Fan票 的详情页中
-如有其他问题请在 瞬Matataki 的官方 TG 群中询问 https://t.me/smartsignature_io`);
+如有其他问题请在 瞬Matataki 的[官方 TG 群](https://t.me/smartsignature_io)询问`, { parse_mode: 'Markdown', disable_web_page_preview: true });
         });
 
         this.processControllers(controllers);
