59a98a90bbe99da6b48d4661fe61e1ed13c3af18
神樹桜乃 <kodama@sakuno.moe>
2020-01-09 20:25:20 +0800

    Use allSettled instead

--- src/services/IBotService.ts
+++ src/services/IBotService.ts
@@ -1,11 +1,13 @@
 import { Group } from "#/entities";
 
+import { Telegram } from "telegraf";
 import { Chat, User, ChatMember, Message } from "telegraf/typings/telegram-types";
 
 export interface IBotService {
     readonly isRunning: boolean;
 
     readonly info: User;
+    readonly api: Telegram;
 
     run(): Promise<void>;
 
