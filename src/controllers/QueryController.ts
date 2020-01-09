72dae5d68db6e8fa6c44959d73d7c52c13b79777
神樹桜乃 <kodama@sakuno.moe>
2020-01-09 18:21:40 +0800

    Use allSettled instead

--- src/controllers/QueryController.ts
+++ src/controllers/QueryController.ts
@@ -6,6 +6,7 @@ import { Injections, LogCategories } from "#/constants";
 import { User, Group } from "#/entities";
 import { IUserRepository, IGroupRepository } from "#/repositories";
 import { IMatatakiService, IBotService, ILoggerService } from "#/services";
+import { allPromiseSettled } from "#/utils";
 
 import { BaseController } from ".";
 
@@ -39,12 +40,12 @@ export class QueryController extends BaseController<QueryController> {
         }
 
         if (info.user) {
-            array.push("");
-
             const user = await this.userRepo.getUser(id);
 
+            const joinedGroupsArray = new Array<string>();
+
             if (!user || user.groups.length === 0) {
-                array.push("*您尚未加入 Fan票 群*");
+                joinedGroupsArray.push("*您尚未加入 Fan票 群*");
             } else {
                 const symbolMap = new Map<number, string>();
                 for (const group of user.groups) {
@@ -57,43 +58,58 @@ export class QueryController extends BaseController<QueryController> {
                     symbolMap.set(group.tokenId, info.minetoken!.symbol);
                 }
 
-                array.push(`*您已加入 ${user.groups.length} 个 Fan票 群*`);
-                const groupInfos = await this.botService.getGroupInfos(user.groups);
-                for (let i = 0; i < user.groups.length; i++) {
-                    try {
-                        const group = user.groups[i];
-                        const groupInfo = groupInfos[i];
-                        const inviteLink = groupInfo.invite_link ?? await telegram.exportChatInviteLink(group.id);
-                        const requiredAmount = group.requirement.minetoken?.amount ?? 0;
+                const results = await allPromiseSettled(user.groups.map(async group => {
+                    const info = await telegram.getChat(Number(group.id));
+                    const inviteLink = info.invite_link ?? await telegram.exportChatInviteLink(group.id);
+                    const requiredAmount = group.requirement.minetoken?.amount ?? 0;
 
-                        array.push(`/ [${groupInfo.title ?? groupInfo.id}](${inviteLink}) （${requiredAmount > 0 ? `${symbolMap.get(group.tokenId)} ≥ ${requiredAmount}` : "暂无规则"}）`);
-                    } catch (e) {
-                        this.loggerService.error(LogCategories.TelegramUpdate, e);
+                    return `/ [${info.title ?? info.id}](${inviteLink}) （${requiredAmount > 0 ? `${symbolMap.get(group.tokenId)} ≥ ${requiredAmount}` : "暂无规则"}）`;
+                }));
+
+                for (const result of results) {
+                    if (result.status === "rejected") {
+                        this.loggerService.error(LogCategories.TelegramUpdate, result.reason);
+                        continue;
                     }
+
+                    joinedGroupsArray.push(result.value);
                 }
+
+                joinedGroupsArray.unshift(`*您已加入 ${joinedGroupsArray.length} 个 Fan票 群*`);
             }
 
-            array.push("");
+            const createdGroupsArray = new Array<string>();
 
             const myGroups = await this.groupRepo.getGroupsOfCreator(id);
             if (myGroups.length === 0) {
-                array.push("*您尚未建立 Fan票 群*");
+                createdGroupsArray.push("*您尚未建立 Fan票 群*");
             } else {
-                array.push(`*您已建立 ${myGroups.length} 个 Fan票 群*`);
-                const groupInfos = await this.botService.getGroupInfos(myGroups);
-                for (let i = 0; i < myGroups.length; i++) {
-                    try {
-                        const group = myGroups[i];
-                        const groupInfo = groupInfos[i];
-                        const inviteLink = groupInfo.invite_link ?? await telegram.exportChatInviteLink(group.id);
-                        const requiredAmount = group.requirement.minetoken?.amount ?? 0;
+                const results = await allPromiseSettled(myGroups.map(async group => {
+                    const groupInfo = await telegram.getChat(Number(group.id));
+                    const inviteLink = groupInfo.invite_link ?? await telegram.exportChatInviteLink(group.id);
+                    const requiredAmount = group.requirement.minetoken?.amount ?? 0;
 
-                        array.push(`/ [${groupInfo.title ?? groupInfo.id}](${inviteLink}) （${requiredAmount > 0 ? `${info.minetoken!.symbol} ≥ ${requiredAmount}` : "暂无规则"}）`);
-                    } catch (e) {
-                        this.loggerService.error(LogCategories.TelegramUpdate, e);
+                    return `/ [${groupInfo.title ?? groupInfo.id}](${inviteLink}) （${requiredAmount > 0 ? `${info.minetoken!.symbol} ≥ ${requiredAmount}` : "暂无规则"}）`;
+                }));
+
+                for (const result of results) {
+                    if (result.status === "rejected") {
+                        this.loggerService.error(LogCategories.TelegramUpdate, result.reason);
+                        continue;
+                    }
+
+                    if (result.value) {
+                        createdGroupsArray.push(result.value);
                     }
                 }
+
+                createdGroupsArray.unshift(`*您已建立 ${createdGroupsArray.length} 个 Fan票 群*`);
             }
+
+            array.push("");
+            array.push(...joinedGroupsArray);
+            array.push("");
+            array.push(...createdGroupsArray);
         }
 
         array.push("");
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,23 @@ export function delay(ms: number) {
         }, ms);
     });
 }
+
+interface PromiseFulfilledResult<T> {
+    status: "fulfilled";
+    value: T;
+}
+interface PromiseRejectedResult {
+    status: "rejected";
+    reason: any;
+}
+type PromiseSettledResult<T> = PromiseFulfilledResult<T> | PromiseRejectedResult;
+
+export function allPromiseSettled<T>(values: Array<Promise<T>>): Promise<Array<PromiseSettledResult<T>>> {
+    return Promise.all<PromiseSettledResult<T>>(values.map(promise => promise.then(value => <PromiseFulfilledResult<T>>({
+        status: "fulfilled",
+        value,
+    })).catch((reason: any) => <PromiseRejectedResult>({
+        status: "rejected",
+        reason,
+    }))));
+}
