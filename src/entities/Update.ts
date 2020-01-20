a3e3fc541d7055ebade0f395fc4fc5d9b381b0a9
神樹桜乃 <kodama@sakuno.moe>
2020-01-20 14:14:45 +0800

    Replace decorators of json columns

--- src/entities/Update.ts
+++ src/entities/Update.ts
@@ -1,10 +1,12 @@
 import { Entity, PrimaryColumn, Column, Index } from "typeorm";
 
+import { JsonColumn } from "#/decorators";
+
 @Entity()
 export class Update {
     @PrimaryColumn({ type: "int" })
     id!: number;
 
-    @Column({ type: "jsonb" })
+    @JsonColumn()
     content!: any;
 }
