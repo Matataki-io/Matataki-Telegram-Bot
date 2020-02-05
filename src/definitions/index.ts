533783653c13cf500ce938951e9593af642da629
神樹桜乃 <kodama@sakuno.moe>
2020-02-05 21:49:28 +0800

    Update /query & /transfer

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -3,5 +3,6 @@ export * from "./AssociatedInfo";
 export * from "./CommandHandlerInfo";
 export * from "./EventHandlerInfo";
 export * from "./GroupRequirement";
+export * from "./MatatakiUserInfo";
 export * from "./MessageHandler";
 export * from "./MinetokenInfo";
