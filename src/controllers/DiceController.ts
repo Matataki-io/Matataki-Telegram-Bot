6addef9be9e09da0e43cb2d8a5e2920de9497743
xiaodao <lychees67@gmail.com>
2020-02-09 18:39:09 -0500

    Export DiceController.

--- src/controllers/DiceController.ts
+++ src/controllers/DiceController.ts
@@ -13,15 +13,19 @@ export class DiceController extends BaseController<DiceController>{
   constructor(@inject(Injections.MatatakiService) private matatakiService: IMatatakiService) {
     super();
   }
+  @Command('new_game', { ignorePrefix: true })
+  async dice(ctx: MessageHandlerContext) {
+  }  
+  @Command('join', { ignorePrefix: true })
+  async join(ctx: MessageHandlerContext) {
+  }
   @Command('roll', { ignorePrefix: true })
   async roll(ctx: MessageHandlerContext) {
     await ctx.replyWithMarkdown(Msgs.rollMsg(_.random(1, 99)));
   }
-  @Command('new_game_roll', { ignorePrefix: true })
-  async dice(ctx: MessageHandlerContext) {
-    
-  }
-
+  @Command('close', { ignorePrefix: true })
+  async close(ctx: MessageHandlerContext) {
+  }  
   private parseDiceArguments(txt :string) {
     const match = txt.match(/^\/new_game_roll(?:@[\w_]+)?\s+(\d*\.?\d*)\s+(\w+)/);
   }
