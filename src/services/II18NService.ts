8410ed0b3169fa7d061c0a6301de06be33b64fde
神樹桜乃 <kodama@sakuno.moe>
2020-02-17 18:39:52 +0800

    Try to fix

--- src/services/II18NService.ts
+++
@@ -1,8 +0,0 @@
-import { Middleware, ContextMessageUpdate } from "telegraf";
-
-export interface II18nService {
-    getInstalledLanguages(): Array<string>;
-
-    t(language: string, key: string): string;
-    middleware<T extends ContextMessageUpdate>(): Middleware<T>;
-}
