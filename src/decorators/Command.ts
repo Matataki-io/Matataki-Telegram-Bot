462bfa1d9b63a013efb829da957d4908c6d8caa6
神樹桜乃 <kodama@sakuno.moe>
2020-02-25 21:58:30 +0800

    Preparation for the new command definition

--- src/decorators/Command.ts
+++ src/decorators/Command.ts
@@ -1,13 +1,36 @@
-import { MetadataKeys } from "../constants";
-import { CommandHandlerInfo } from "#/definitions";
+import { MetadataKeys } from "#/constants";
+import { CommandHandlerInfo, I18nContext } from "#/definitions";
 
 type CommandBindingOptions = {
     ignorePrefix?: boolean;
 }
 
-export function Command(name: string, options?: CommandBindingOptions): MethodDecorator {
-    return (target: Object, methodName: string | Symbol) => {
-        if (methodName instanceof Symbol) {
+function Command(name: string, options?: CommandBindingOptions): MethodDecorator;
+function Command(name: string, argumentRegex: RegExp, options?: CommandBindingOptions): MethodDecorator;
+function Command(name: string, argumentRegex: RegExp, errorMessage: string | ((i18n: I18nContext) => string), options?: CommandBindingOptions): MethodDecorator;
+function Command(name: string,
+    firstArg?: RegExp | CommandBindingOptions,
+    secondArg?: string | ((i18n: I18nContext) => string) | CommandBindingOptions,
+    thirdArg?: CommandBindingOptions): MethodDecorator {
+    let argumentRegex: RegExp | undefined;
+    let errorMessage: string | ((i18n: I18nContext) => string) | undefined;
+    let options: CommandBindingOptions | undefined;
+
+    if (!(firstArg instanceof RegExp)) {
+        options = firstArg;
+    } else {
+        argumentRegex = firstArg;
+
+        if (typeof secondArg === "string" || typeof secondArg === "function") {
+            errorMessage = secondArg;
+            options = thirdArg;
+        } else {
+            options = secondArg;
+        }
+    }
+
+    return (target: Object, methodName: string | symbol) => {
+        if (typeof methodName === "symbol") {
             throw new Error("Impossible situation");
         }
 
@@ -19,7 +42,13 @@ export function Command(name: string, options?: CommandBindingOptions): MethodDe
         commands.push({
             name,
             methodName,
+            argumentRegex,
+            errorMessage,
             ignorePrefix: options?.ignorePrefix ?? false,
         });
     };
 }
+
+export {
+    Command,
+};
