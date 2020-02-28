71f1361e49795dc4bb3e5909978109d9595c48a0
神樹桜乃 <kodama@sakuno.moe>
2020-02-28 16:53:02 +0800

    It's allowed to pass multiple permissions

--- src/decorators/RequirePermissions.ts
+++ src/decorators/RequirePermissions.ts
@@ -1,12 +1,18 @@
 import { MessageHandlerContext, ControllerMethodContext } from "#/definitions";
 import { IBotService } from "#/services";
 import { MetadataKeys, Injections } from "#/constants";
+type HandlerFunc = (ctx: MessageHandlerContext, ...args: any[]) => any;
+
 
 type Permissions = "can_be_edited" | "can_post_messages" | "can_edit_messages" | "can_delete_messages" | "can_restrict_members" | "can_promote_members" | "can_change_info" | "can_invite_users" | "can_pin_messages";
 
-type HandlerFunc = (ctx: MessageHandlerContext, ...args: any[]) => any;
+function RequirePermissions(permission: Permissions): MethodDecorator;
+function RequirePermissions(permissions: Array<Permissions>): MethodDecorator;
+function RequirePermissions(permissions: Permissions | Array<Permissions>): MethodDecorator {
+    if (!Array.isArray(permissions)) {
+        permissions = [permissions];
+    }
 
-export function RequrePermissions(permissions: Array<Permissions>): MethodDecorator {
     return (target: Object, methodName: string | symbol, descriptor: PropertyDescriptor) => {
         const decoratedMethod = <HandlerFunc>descriptor.value;
 
@@ -19,13 +25,13 @@ export function RequrePermissions(permissions: Array<Permissions>): MethodDecora
             const info = await getChatMember(ctx.chat!.id, botService.info.id);
 
             if (info.status !== "administrator") {
-                await ctx.reply("");
+                await ctx.reply(ctx.i18n.t("error.notadmin"));
                 return;
             }
 
-            for (const permission of permissions) {
+            for (const permission of <Array<Permissions>>permissions) {
                 if (info[permission] !== true) {
-                    await ctx.reply("");
+                    await ctx.reply(ctx.i18n.t("error.nopermission"));
                     return;
                 }
             }
@@ -33,4 +39,8 @@ export function RequrePermissions(permissions: Array<Permissions>): MethodDecora
             return decoratedMethod.call(this, ctx, ...args);
         };
     };
+}
+
+export {
+    RequirePermissions,
 }
