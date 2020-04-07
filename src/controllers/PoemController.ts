d45f42a51799ad6e03625dc588ef3778e5bb747b
神樹桜乃 <kodama@sakuno.moe>
2020-04-07 19:18:46 +0800

    Make poem generating friendly

--- src/controllers/PoemController.ts
+++ src/controllers/PoemController.ts
@@ -13,7 +13,17 @@ export class PoemController extends BaseController<PoemController> {
     }
 
     @Command("make", /(\p{sc=Han}+)$/u)
-    async makePoem({ reply }: MessageHandlerContext, @InjectRegexMatchGroup(1) keyword: string) {
-        await reply(await this.poemService.make(keyword));
+    async makePoem({ message, reply, i18n, telegram }: MessageHandlerContext, @InjectRegexMatchGroup(1) keyword: string) {
+        const poemMessage = await reply(i18n.t("poem.generating"), {
+            reply_to_message_id: message.chat.type !== "private" ? message.message_id : undefined,
+        });
+
+        try {
+            const result = await this.poemService.make(keyword);
+
+            await telegram.editMessageText(message.chat.id, poemMessage.message_id, undefined, result);
+        } catch (e) {
+            await telegram.editMessageText(message.chat.id, poemMessage.message_id, undefined, e.message);
+        }
     }
 }
