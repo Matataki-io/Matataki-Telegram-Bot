ebcc70ea8ed38a89667eb04377fe37d78ef2c827
神樹桜乃 <kodama@sakuno.moe>
2020-06-14 21:02:42 +0800

    Support handler declaration order

--- src/MatatakiBot/Abstract/CommandHandlerAttribute.cs
+++ src/MatatakiBot/Abstract/CommandHandlerAttribute.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace MatatakiBot.Abstract
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public sealed class CommandHandlerAttribute : Attribute
     {
+        public int Order { get; }
+
+        public CommandHandlerAttribute([CallerLineNumber] int order = 0)
+        {
+            Order = order;
+        }
     }
 }
