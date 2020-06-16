1eb98c3bc8a184e79fb3740d382458b84e7353ec
神樹桜乃 <kodama@sakuno.moe>
2020-06-16 23:09:21 +0800

    Register middleware type to container

--- src/MatatakiBot/Bot.cs
+++ src/MatatakiBot/Bot.cs
@@ -1,4 +1,4 @@
-using DryIoc;
+﻿using DryIoc;
 using MatatakiBot.Abstract;
 using MatatakiBot.Core;
 using System;
@@ -14,11 +14,13 @@ namespace MatatakiBot
 
         private readonly Container _container = new Container();
 
+        private readonly List<Type> _middlewareTypes = new List<Type>();
         private readonly CommandDispatcher _commandDispatcher;
 
         public Bot(ITelegramBotClient client)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
+            _container.RegisterInstance(typeof(ITelegramBotClient), _client);
 
             _commandDispatcher = new CommandDispatcher(_container);
         }
@@ -35,6 +37,18 @@ namespace MatatakiBot
             _container.Register<CommandBase, T>(serviceKey: commandAttribute.Name);
 
             _commandDispatcher.Register(commandAttribute.Name, typeof(T));
+        }
+
+        public void RegisterCommandMiddleware<T>() where T : ICommandMiddleware
+        {
+            var type = typeof(T);
+
+            if (_middlewareTypes.Contains(type))
+                throw new ArgumentException("Provided middleware is registered");
+
+            _middlewareTypes.Add(type);
+
+            _container.Register(type);
         }
     }
 }
