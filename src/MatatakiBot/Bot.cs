42f32ed1f9b7b546bae5bb73cf998dee2e0c9c16
神樹桜乃 <kodama@sakuno.moe>
2020-06-18 14:32:02 +0800

    Build message pipeline

--- src/MatatakiBot/Bot.cs
+++ src/MatatakiBot/Bot.cs
@@ -1,12 +1,18 @@
-using DryIoc;
+﻿using DryIoc;
 using MatatakiBot.Abstract;
 using MatatakiBot.Core;
 using Serilog;
 using Serilog.Events;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace MatatakiBot
 {
@@ -18,9 +24,13 @@ namespace MatatakiBot
 
         private readonly ILogger _logger;
 
+        private readonly ResponseSender _responseSender;
+        private readonly MessageDispatcher _messageDispatcher;
+
         private readonly List<Type> _middlewareTypes = new List<Type>();
         private readonly List<IMessageMiddleware> _middlewares = new List<IMessageMiddleware>();
-        private readonly MessageDispatcher _messageDispatcher;
+
+        private bool _isStarted;
 
         public Bot(ITelegramBotClient client)
         {
@@ -38,6 +48,7 @@ namespace MatatakiBot
                 .CreateLogger();
             _container.RegisterInstance(_logger);
 
+            _responseSender = new ResponseSender(_client);
             _messageDispatcher = new MessageDispatcher(_container);
         }
 
@@ -67,6 +78,78 @@ namespace MatatakiBot
             _middlewareTypes.Add(type);
 
             _container.Register(type);
+        }
+
+        public async ValueTask StartReceiving(CancellationToken cancellationToken = default)
+        {
+            if (_isStarted)
+                throw new InvalidOperationException("The bot has started");
+
+            _middlewares.Clear();
+
+            _middlewares.Add(_responseSender);
+            foreach (var type in _middlewareTypes)
+                _middlewares.Add(_container.Resolve<IMessageMiddleware>(type));
+            _middlewares.Add(_messageDispatcher);
+
+            var botInfo = await _client.GetMeAsync(cancellationToken);
+
+            _messageDispatcher.Username = botInfo.Username;
+
+            _isStarted = true;
+
+            cancellationToken.Register(() => _isStarted = false);
+
+            var updater = new BlockingUpdateReceiver(_client, new[] { UpdateType.Message }, cancellationToken: cancellationToken);
+
+            await foreach (var update in updater.YieldUpdatesAsync())
+            {
+                _ = update.Type switch
+                {
+                    UpdateType.Message => HandleMessage(update.Message),
+
+                    _ => throw new InvalidOperationException("Unexpected update type"),
+                };
+            }
+        }
+
+        internal async Task HandleMessage(Message message)
+        {
+            try
+            {
+                await foreach (var _ in Execute(message, 0)) ;
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Something went wrong in message handling pipeline");
+            }
+
+            IAsyncEnumerable<MessageResponse> Execute(Message message, int index)
+            {
+                try
+                {
+                    var middleware = _middlewares[index];
+
+                    var isNextHandlerCalled = false;
+
+                    return middleware.HandleMessageAsync(message, msg =>
+                    {
+                        if (isNextHandlerCalled)
+                            throw new InvalidOperationException("Don't call nextHandler multiple times");
+
+                        isNextHandlerCalled = true;
+
+                        if (index >= _middlewares.Count)
+                            throw new InvalidOperationException("Don't call nextHandler in MessageDispatcher");
+
+                        return Execute(msg ?? message, index + 1);
+                    });
+                }
+                catch (Exception e)
+                {
+                    return Task.FromException<MessageResponse>(e).ToAsyncEnumerable();
+                }
+            }
         }
     }
 }
