4adbeecbeabfa223c3d9a5aa6422d822d440a8c1
神樹桜乃 <kodama@sakuno.moe>
2020-11-20 19:13:13 +0800

    Add deep equality support to I18n type

--- src/MatatakiBot/Core/I18n.cs
+++ src/MatatakiBot/Core/I18n.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using KellermanSoftware.CompareNetObjects;
+using System;
 
 namespace MatatakiBot.Core
 {
-    public sealed class I18n
+    public sealed class I18n : IEquatable<I18n>
     {
+        private static CompareLogic _compareLogic;
+
         public string Key { get; }
         public object? Arguments { get; }
 
+        static I18n()
+        {
+            _compareLogic = new CompareLogic(new ComparisonConfig()
+            {
+                IgnoreObjectTypes = true,
+            });
+        }
         public I18n(string key) : this(key, null) { }
         public I18n(string key, object? args)
         {
@@ -15,6 +25,14 @@ namespace MatatakiBot.Core
 
             Key = key;
             Arguments = args;
+        }
+
+        public bool Equals(I18n? other)
+        {
+            if (other is null || Key != other.Key)
+                return false;
+
+            return _compareLogic.Compare(Arguments, other.Arguments).AreEqual;
         }
     }
 }
