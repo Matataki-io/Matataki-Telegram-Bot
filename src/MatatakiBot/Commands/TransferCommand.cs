cecce63eaf0d9a1acc23dbbbcf030975cbc19789
神樹桜乃 <kodama@sakuno.moe>
2020-11-20 20:19:42 +0800

    Add transfer command

---
+++ src/MatatakiBot/Commands/TransferCommand.cs
@@ -0,0 +1,68 @@
+﻿using MatatakiBot.Abstract;
+using MatatakiBot.Core;
+using MatatakiBot.Services;
+using MatatakiBot.Types;
+using System.Collections.Generic;
+using Telegram.Bot.Types;
+
+namespace MatatakiBot.Commands
+{
+    [Command("transfer")]
+    class TransferCommand : CommandBase
+    {
+        private readonly IBackendService _backendService;
+        private readonly IMatatakiService _matatakiService;
+
+        public TransferCommand(IBackendService backendService, IMatatakiService matatakiService)
+        {
+            _backendService = backendService;
+            _matatakiService = matatakiService;
+        }
+
+        [CommandHandler(@"(\d+)\s+(\w+)\s+(\d+.?\d*)")]
+        public async IAsyncEnumerable<MessageResponse> TransferByMatatakiId(Message message, int receiverId, string symbol, decimal amount)
+        {
+            var sender = await _backendService.GetUserByTelegramIdAsync(message.From.Id);
+            var receiver = await _backendService.GetUserAsync(receiverId);
+
+            await foreach (var response in TransferCoreAsync(sender, receiver, amount, symbol))
+                yield return response;
+        }
+
+        private async IAsyncEnumerable<MessageResponse> TransferCoreAsync(UserInfo sender, UserInfo receiver, decimal amount, string symbol)
+        {
+            symbol = symbol.ToUpperInvariant();
+
+            var content = new I18n("wallet.transfer.common", new
+            {
+                senderUsername = sender.Name,
+                senderUrl = _matatakiService.GetUserPageUrl(sender.Id),
+                receiverUsername = receiver.Name,
+                receiverUrl = _matatakiService.GetUserPageUrl(receiver.Id),
+                amount, symbol,
+            });
+
+            yield return new MessageResponse(new I18n("wallet.transfer.started"), content);
+
+            MessageResponse finalResponse;
+
+            try
+            {
+                var hash = await _matatakiService.TransferAsync(sender.Id, receiver.Id, amount, symbol);
+
+                finalResponse = new MessageResponse(new I18n("wallet.transfer.successful"), content).WithInlineButtons(
+                    InlineButton.WithUrl(new I18n("wallet.transfer.transactionDetail"), "https://rinkeby.etherscan.io/tx/" + hash)
+                );
+            }
+            catch
+            {
+                finalResponse = new MessageResponse(new I18n("wallet.transfer.failed"), content);
+            }
+
+            yield return finalResponse;
+        }
+
+        [CommandHandler]
+        public MessageResponse Fallback() => "Bad format";
+    }
+}
---
+++ tests/MatatakiBot.Tests/TransferCommandTests.cs
@@ -0,0 +1,46 @@
+﻿using MatatakiBot.Commands;
+using MatatakiBot.Core;
+using MatatakiBot.Services;
+using MatatakiBot.Types;
+using NSubstitute;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Xunit;
+
+namespace MatatakiBot.Tests
+{
+    public class TransferCommandTests
+    {
+        [Fact]
+        public async Task TransferByMatatakiId()
+        {
+            var backendService = Substitute.For<IBackendService>();
+            var matatakiService = Substitute.For<IMatatakiService>();
+
+            backendService.GetUserByTelegramIdAsync(1).Returns(new UserInfo() { Id = 123, Name = "Sender" });
+            backendService.GetUserAsync(1234).Returns(new UserInfo() { Id = 1234, Name = "Receiver" });
+            matatakiService.TransferAsync(123, 1234, 100, "SYMBOL").Returns("0x123456789ABCDEF");
+            matatakiService.GetUserPageUrl(Arg.Any<int>()).Returns(info => "https://matataki/user/" + info[0]);
+
+            var command = new TransferCommand(backendService, matatakiService);
+
+            var responses = await command.TransferByMatatakiId(new Message()
+            {
+                From = new User() { Id = 1 }
+            }, 1234, "SYMBOL", 100).ToArrayAsync();
+
+            Assert.Equal(2, responses.Length);
+
+            Assert.Equal(new I18n("wallet.transfer.started"), (I18n)responses[0].Introduction!);
+            Assert.Equal(new I18n("wallet.transfer.common", new
+            {
+                senderUsername = "Sender",
+                senderUrl = "https://matataki/user/123",
+                receiverUsername = "Receiver",
+                receiverUrl = "https://matataki/user/1234",
+                amount = 100m, symbol = "SYMBOL",
+            }), (I18n)responses[0].Content);
+        }
+    }
+}
