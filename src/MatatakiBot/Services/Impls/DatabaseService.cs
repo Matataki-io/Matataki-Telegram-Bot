07e0a0012eb8566f52de8d988b9b7f590dec017e
神樹桜乃 <kodama@sakuno.moe>
2020-11-20 20:21:08 +0800

    Update

--- src/MatatakiBot/Services/Impls/DatabaseService.cs
+++ src/MatatakiBot/Services/Impls/DatabaseService.cs
@@ -29,7 +29,7 @@ namespace MatatakiBot.Services.Impls
 
                 _clientCertCallback = certs =>
                 {
-                    certs.Add(new X509Certificate2(database.CertificateFile));
+                    certs.Add(new X509Certificate2(database.CertificateFile ?? throw new InvalidOperationException("Missing Database.CertificateFile in app settings")));
                 };
             }
 
