7643ea16144e922827491df9e6fe80481e06d83a
神樹桜乃 <kodama@sakuno.moe>
2021-04-02 20:40:51 +0800

    Update unit tests

---
+++ tests/MatatakiBot.Tests/StartCommandTests.cs
@@ -0,0 +1,30 @@
+﻿using MatatakiBot.Commands;
+using MatatakiBot.Services;
+using NSubstitute;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Xunit;
+
+namespace MatatakiBot.Tests
+{
+    public class StartCommandTests
+    {
+        [Fact]
+        public async Task InvalidGroupId()
+        {
+            var groupService = Substitute.For<IGroupService>();
+            var command = new StartCommand(groupService, Substitute.For<ITelegramBotClient>());
+
+            var response = command.GroupHandler(new Message(), -404).GetAsyncEnumerator();
+
+            Assert.True(await response.MoveNextAsync());
+            Assert.Equal("查询中……", response.Current.Content);
+
+            Assert.True(await response.MoveNextAsync());
+            Assert.Equal("没有这样的群", response.Current.Content);
+
+            Assert.False(await response.MoveNextAsync());
+        }
+    }
+}
---
+++ tests/MatatakiBot.Tests/SyncUsernameTests.cs
@@ -0,0 +1,41 @@
+﻿using MatatakiBot.Commands;
+using MatatakiBot.Services;
+using NSubstitute;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Xunit;
+
+namespace MatatakiBot.Tests
+{
+    public class SyncUsernameTests
+    {
+        [Fact]
+        public async Task NoUsername()
+        {
+            var command = new SyncUsernameCommand(Substitute.For<IUserService>());
+
+            var response = await command.Handler(new Message()
+            {
+                From = new User() { Id = 1234 },
+            });
+
+            Assert.Equal("您的帐号缺少用户名。请进入个人页面设置用户名。", response.Content);
+        }
+
+        [Fact]
+        public async Task Updated()
+        {
+            var service = Substitute.For<IUserService>();
+            var command = new SyncUsernameCommand(service);
+
+            var response = await command.Handler(new Message()
+            {
+                From = new User() { Id = 1234, Username = "username" },
+            });
+
+            await service.Received().SetUsernameAsync(1234, "username");
+
+            Assert.Equal("Ok", response.Content);
+        }
+    }
+}
