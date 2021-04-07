d4a1fa8d3c38555fe85aebb6aba4d5c975e77e77
神樹桜乃 <kodama@sakuno.moe>
2021-04-08 03:36:49 +0800

    Fix hongbao creation

---
+++ src/MatatakiBot/Actions/CreateHongbaoAction.cs
@@ -0,0 +1,99 @@
+﻿using MatatakiBot.Attributes;
+using MatatakiBot.Services;
+using MatatakiBot.Types;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace MatatakiBot.Actions
+{
+    [Action("createHongbao")]
+    class CreateHongbaoAction : ActionBase
+    {
+        private static readonly Regex _amountAndSymbolRegex = new(@"(\d+.?\d*) (\w+)", RegexOptions.Compiled);
+
+        private readonly ITelegramBotClient _botClient;
+        private readonly IBackendService _backendService;
+        private readonly IPrivateChatService _privateChatService;
+        private readonly IHongbaoService _hongbaoService;
+
+        public CreateHongbaoAction(ITelegramBotClient botClient, IPrivateChatService privateChatService, IBackendService backendService, IHongbaoService hongbaoService)
+        {
+            _botClient = botClient;
+            _privateChatService = privateChatService;
+            _backendService = backendService;
+            _hongbaoService = hongbaoService;
+        }
+
+        [ActionHandler(@"(\d+) (-\d+)")]
+        public async Task Handler(CallbackQuery callbackQuery, long senderId, long targetGroupId)
+        {
+            await _botClient.EditMessageTextAsync(callbackQuery.InlineMessageId, @"请输入红包的内容
+
+格式：`[数额] [Fan 票符号]`", ParseMode.Markdown);
+
+            decimal amount;
+            string symbol;
+
+            do
+            {
+                var input = await _privateChatService.ReadUserInputAsync(callbackQuery.From.Id);
+
+                var match = _amountAndSymbolRegex.Match(input);
+                if (match.Success)
+                {
+                    amount = decimal.Parse(match.Groups[1].Value);
+                    symbol = match.Groups[2].Value;
+                    break;
+                }
+
+                await _botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, @"格式错误，应为：`[数额] [Fan 票符号]`", ParseMode.Markdown);
+            } while (true);
+
+            await _botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "请输入红包的份数");
+
+            int count;
+
+            do
+            {
+                var input = await _privateChatService.ReadUserInputAsync(callbackQuery.From.Id);
+
+                if (int.TryParse(input, out count))
+                {
+                    if (count <= 0)
+                    {
+                        await _botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "必须为大于 0 的数目");
+                        continue;
+                    }
+
+                    break;
+                }
+
+                await _botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "格式错误，应为：`数字`", ParseMode.Markdown);
+            } while (true);
+
+            await _botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "准备红包中……");
+
+            try
+            {
+                await _backendService.GetTokenAsync(symbol);
+            }
+            catch (TokenNotFoundException)
+            {
+                throw new HandlerException("抱歉，没有这样的 Fan 票");
+            }
+
+            var senderInfo = await _backendService.GetUserByTelegramIdAsync(callbackQuery.From.Id); ;
+            var id = await _hongbaoService.CreateAsync(senderInfo.Id, targetGroupId, amount, symbol, count);
+
+            await _botClient.SendTextMessageAsync(targetGroupId, $"@{callbackQuery.From.Username} 发了个 {symbol} 红包",
+                replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("获取", $"grabHongbao {id}")));
+
+            await _botClient.SendTextMessageAsync(callbackQuery.Message.Chat.Id, "Ok");
+        }
+    }
+}
--- src/MatatakiBot/Commands/HongbaoCommand.cs
+++ src/MatatakiBot/Commands/HongbaoCommand.cs
@@ -2,13 +2,10 @@
 using MatatakiBot.Services;
 using MatatakiBot.Types;
 using Serilog;
-using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.ReplyMarkups;
 
 namespace MatatakiBot.Commands
 {
@@ -16,31 +13,20 @@ namespace MatatakiBot.Commands
     [Command("hongbao")]
     class HongbaoCommand : CommandBase
     {
-        private static readonly Regex _amountAndSymbolRegex = new(@"(\d+.?\d*) (\w+)", RegexOptions.Compiled);
-
         private readonly ITelegramBotClient _botClient;
         private readonly ILogger _logger;
-        private readonly IPrivateChatService _privateChatService;
         private readonly IBackendService _backendService;
         private readonly IGroupService _groupService;
-        private readonly ICallbackQueryService _callbackQueryService;
-        private readonly IHongbaoService _hongbaoService;
 
         public HongbaoCommand(ITelegramBotClient botClient,
             ILogger logger,
-            IPrivateChatService privateChatService,
             IBackendService backendService,
-            IGroupService groupService,
-            ICallbackQueryService callbackQueryService,
-            IHongbaoService hongbaoService)
+            IGroupService groupService)
         {
             _botClient = botClient;
             _logger = logger;
-            _privateChatService = privateChatService;
             _backendService = backendService;
             _groupService = groupService;
-            _callbackQueryService = callbackQueryService;
-            _hongbaoService = hongbaoService;
         }
 
         [CommandHandler]
@@ -66,7 +52,7 @@ namespace MatatakiBot.Commands
                 {
                     var chat = await _botClient.GetChatAsync(groupId);
 
-                    groupButtons.Add(new[] { InlineButton.WithCallbackData(chat.Title, $"hongbao|{message.From.Id}|{chat.Id}") });
+                    groupButtons.Add(new[] { InlineButton.WithCallbackData(chat.Title, $"createHongbao {message.From.Id} {chat.Id}") });
                 }
                 catch (ChatNotFoundException)
                 {
@@ -75,71 +61,6 @@ namespace MatatakiBot.Commands
             }
 
             yield return Markdown("请选择你要发红包的群").WithInlineButtons(groupButtons);
-
-            var prefix = $"hongbao|{message.From.Id}";
-            var targetGroupId = long.Parse((await _callbackQueryService.WaitForCallbackQueryAsync(prefix)).AsSpan(prefix.Length + 1));
-
-            yield return Markdown(@"请输入红包的内容
-
-格式：`[数额] [Fan 票符号]`");
-
-            decimal amount;
-            string symbol;
-
-            do
-            {
-                var input = await _privateChatService.ReadUserInputAsync(message.From.Id);
-
-                var match = _amountAndSymbolRegex.Match(input);
-                if (match.Success)
-                {
-                    amount = decimal.Parse(match.Groups[1].Value);
-                    symbol = match.Groups[2].Value;
-                    break;
-                }
-
-                yield return Markdown(@"格式错误，应为：`[数额] [Fan 票符号]`").WithForceNewMessage();
-            } while (true);
-
-            yield return Markdown(@"请输入红包的份数").WithForceNewMessage();
-
-            int count;
-
-            do
-            {
-                var input = await _privateChatService.ReadUserInputAsync(message.From.Id);
-
-                if (int.TryParse(input, out count))
-                {
-                    if (count <= 0)
-                    {
-                        yield return Text("必须为大于 0 的数目").WithForceNewMessage();
-                        continue;
-                    }
-
-                    break;
-                }
-
-                yield return Markdown(@"格式错误，应为：`[数额] [Fan 票符号]`").WithForceNewMessage();
-            } while (true);
-
-            yield return Text("准备红包中……").WithForceNewMessage();
-
-            try
-            {
-                await _backendService.GetTokenAsync(symbol);
-            }
-            catch (TokenNotFoundException)
-            {
-                throw new HandlerException("抱歉，没有这样的 Fan 票");
-            }
-
-            var id = await _hongbaoService.CreateAsync(senderInfo.Id, targetGroupId, amount, symbol, count);
-
-            await _botClient.SendTextMessageAsync(targetGroupId, $"@{message.From.Username} 发了个 {symbol} 红包",
-                replyMarkup: new InlineKeyboardMarkup(InlineKeyboardButton.WithCallbackData("获取", $"grabHongbao {id}")));
-
-            yield return Text("Ok");
         }
     }
 }
